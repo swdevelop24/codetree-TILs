8b980fc839c31332181cd03d03621f7b472693ba
swdevelop24 <154391675+swdevelop24@users.noreply.github.com>
2024-08-19 20:43:10 -0500

    Title: 입력받아 계산 5, Time: 138ms, Memory: 24MB, Status: Passed - CodeTree

--- 240820/입력받아 계산 5/input-calculate-5.py
+++ 240820/입력받아 계산 5/input-calculate-5.py
@@ -1,4 +1,11 @@
-a,b = input().split()
-a=int(a)
-b=int(b)
+# a,b = input().split()
+# a=int(a)
+# b=int(b)
+# print(a+b)
+
+inp =input()
+arr =inp.split()
+a=int(arr[0])
+b=int(arr[1])
+
 print(a+b)
