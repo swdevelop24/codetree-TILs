4d77ac78f9d9dd363fafb144d50a2332f2a5cbe2
swdevelop24 <154391675+swdevelop24@users.noreply.github.com>
2024-08-19 22:31:23 -0500

    Title: 실수와 문자 받아 출력하기, Time: 113ms, Memory: 72MB, Status: Passed - CodeTree

--- 240820/실수와 문자 받아 출력하기/print-number--and-letter.py
+++ 240820/실수와 문자 받아 출력하기/print-number--and-letter.py
@@ -2,6 +2,8 @@ c=input()
 a=float(input())
 b=float(input())
 
-print(c)
-print(f"{a:.2f}")
-print(f"{b:.2f}")
+# print(c)
+# print(f"{a:.2f}")
+# print(f"{b:.2f}")
+# 출력
+print(f"{c}\n{a:.2f}\n{b:.2f}")
