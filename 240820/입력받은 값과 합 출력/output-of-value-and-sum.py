2975e0642a1193d6b86ab125e4822bf4896d09c9
swdevelop24 <154391675+swdevelop24@users.noreply.github.com>
2024-08-19 20:49:09 -0500

    Title: 입력받은 값과 합 출력, Time: 145ms, Memory: 24MB, Status: Passed - CodeTree

--- 240820/입력받은 값과 합 출력/output-of-value-and-sum.py
+++ 240820/입력받은 값과 합 출력/output-of-value-and-sum.py
@@ -1,4 +1,12 @@
-a,b = input().split()
-a=int(a)
-b=int(b)
-print(a,b,a+b)
+# a,b = input().split()
+# a=int(a)
+# b=int(b)
+# print(a,b,a+b)
+# 변수 선언, 입력
+inp = input()
+arr = inp.split()
+a = int(arr[0])
+b = int(arr[1])
+
+# 출력
+print(a, b, a + b)
