edfed09a7f1ac513e4e91b4e4a82415edc2cd8a3
swdevelop24 <154391675+swdevelop24@users.noreply.github.com>
2024-08-19 20:46:58 -0500

    Title: 입력받은 값 교체하기, Time: 195ms, Memory: 24MB, Status: Passed - CodeTree

--- 240820/입력받은 값 교체하기/changing-inputs.py
+++ 240820/입력받은 값 교체하기/changing-inputs.py
@@ -1,6 +1,14 @@
-inp = input().split()
-a = inp[0];
-b=inp[1]; 
-a,b = b,a
+# inp = input().split()
+# a = inp[0];
+# b=inp[1]; 
+# a,b = b,a
 
-print(a,b)
+# print(a,b)
+
+inp = input()
+arr = inp.split()
+a = int(arr[0])
+b = int(arr[1])
+
+# 출력
+print(b, a)
