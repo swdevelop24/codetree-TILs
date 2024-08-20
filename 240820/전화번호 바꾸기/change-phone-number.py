f29aa00954a6f8c1d1f2acadc68f8c2f2c896ce6
swdevelop24 <154391675+swdevelop24@users.noreply.github.com>
2024-08-19 23:17:09 -0500

    Title: 전화번호 바꾸기, Time: 117ms, Memory: 25MB, Status: Passed - CodeTree

--- 240820/전화번호 바꾸기/change-phone-number.py
+++ 240820/전화번호 바꾸기/change-phone-number.py
@@ -1,5 +1,16 @@
-inp = input().split("-")
-n= inp[0]
-x=int(inp[1])
-y=int(inp[2])
-print(f"{n}-{y}-{x}")
+# inp = input().split("-")
+# n= inp[0]
+# x=int(inp[1])
+# y=int(inp[2])
+# print(f"{n}-{y}-{x}")
+
+# 가독성이 이게 더 좋음
+# 변수 선언, 입력
+inp = input()
+arr = inp.split("-")
+a = int(arr[0])
+b = int(arr[1])
+c = int(arr[2])
+
+# 출력
+print(f"010-{c}-{b}")
