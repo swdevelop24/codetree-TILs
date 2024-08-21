116761a0501997df81c239c84aa1d74b8d439b65
swdevelop24 <154391675+swdevelop24@users.noreply.github.com>
2024-08-20 22:14:33 -0500

    Title: 합과 평균의 차, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

--- 240821/합과 평균의 차/sub-of-average-and-sum.py
+++ 240821/합과 평균의 차/sub-of-average-and-sum.py
@@ -1,10 +1,23 @@
-arr=input().split()
-a =int(arr[0])
-b =int(arr[1])
-c =int(arr[2])
+# arr=input().split()
+# a =int(arr[0])
+# b =int(arr[1])
+# c =int(arr[2])
 
-s = a+b+c
-a = s//3 
-print(s)
-print(a)
-print(s-a)
+# s = a+b+c
+# a = s//3 
+# print(s)
+# print(a)
+# print(s-a)
+
+
+# 변수 선언, 입력
+inp = input()
+arr = inp.split()
+a = int(arr[0])
+b = int(arr[1])
+c = int(arr[2])
+
+# 출력
+print(a + b + c)
+print((a + b + c) // 3)
+print((a + b + c) - (a + b + c) // 3)
