f381f5ef3c705973c7eebf5801aef1293dfa6cd4
swdevelop24 <154391675+swdevelop24@users.noreply.github.com>
2024-08-20 20:56:56 -0500

    Title: 길이 변형 후 사각형 넓이 구하기, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240821/길이 변형 후 사각형 넓이 구하기/square-width-after-length-change.py
+++ 240821/길이 변형 후 사각형 넓이 구하기/square-width-after-length-change.py
@@ -1,9 +1,18 @@
-inp = input().split()
-w = int(inp[0])
-l = int(inp[1])
-i = w+8 
-c = l*3
-s = i*c
-print(i)
-print(c)
-print(s)
+# inp = input().split()
+# w = int(inp[0])
+# l = int(inp[1])
+# i = w+8 
+# c = l*3
+# s = i*c
+# print(i)
+# print(c)
+# print(s)
+
+# 사칙연산 계산시 type은 더 큰 범위를 따라가게 되어있음
+inp =input().split();
+a = int(inp[0])
+b=int(inp[1]) 
+print(a+b)
+print(a-b)
+print(int(a//b))
+print(int(a%b))
