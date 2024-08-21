91b36711aadae7954f9879a6b3b9198cdf50f7b9
swdevelop24 <154391675+swdevelop24@users.noreply.github.com>
2024-08-20 21:01:11 -0500

    Title: 두수의 곱과 몫, Time: 113ms, Memory: 25MB, Status: Passed - CodeTree

--- 240821/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.py
+++ 240821/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.py
@@ -1,6 +1,15 @@
-inp = input().split()
-a =int(inp[0])
-b=int(inp[1])
+# inp = input().split()
+# a =int(inp[0])
+# b=int(inp[1])
 
-print(f"{a} * {b} = {a*b}")
-print(f"{a} / {b} = {int(a//b)}")
+# print(f"{a} * {b} = {a*b}")
+# print(f"{a} / {b} = {int(a//b)}")
+
+# 변수 선언, 입력
+inp = input()
+arr = inp.split()
+a = int(arr[0])
+b = int(arr[1])
+
+# 출력
+print(f"{a} * {b} = {a * b}\n{a} / {b} = {a // b}")
