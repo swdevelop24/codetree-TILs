4c89551b62139de6c2d6bc0b18dbb12ff59c742a
swdevelop24 <154391675+swdevelop24@users.noreply.github.com>
2024-08-20 21:30:01 -0500

    Title: 2개의 정수를 서로 더하기, Time: 144ms, Memory: 24MB, Status: Passed - CodeTree

--- 240821/2개의 정수를 서로 더하기/add-two-integers-each-other.py
+++ 240821/2개의 정수를 서로 더하기/add-two-integers-each-other.py
@@ -1,7 +1,20 @@
-inp = input().split() 
-a =int(inp[0])
-b=int(inp[1])  
-a+=b
-b+=a
+# inp = input().split() 
+# a =int(inp[0])
+# b=int(inp[1])  
+# a+=b
+# b+=a
 
-print(f"{a} {b}")
+# print(f"{a} {b}")
+
+
+# 변수 선언, 입력
+inp = input()
+arr = inp.split()
+a = int(arr[0])
+b = int(arr[1])
+
+a += b
+b += a
+
+# 출력
+print(a, b)
