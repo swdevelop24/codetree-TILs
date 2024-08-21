52113517e3401910d5494ec72d88c694166bfe70
swdevelop24 <154391675+swdevelop24@users.noreply.github.com>
2024-08-20 21:45:03 -0500

    Title: 합과 차의 나눗셈, Time: 121ms, Memory: 25MB, Status: Passed - CodeTree

--- 240821/합과 차의 나눗셈/divide-of-sum-and-sub.py
+++ 240821/합과 차의 나눗셈/divide-of-sum-and-sub.py
@@ -1,5 +1,13 @@
-arr =input().split()
-a=int(arr[0])
-b=int(arr[1])
-c = (a+b)/(a-b)
-print(f"{c:.2f}")
+# arr =input().split()
+# a=int(arr[0])
+# b=int(arr[1])
+# c = (a+b)/(a-b)
+# print(f"{c:.2f}")
+
+# 변수 선언 및 입력
+inp = input()
+arr = inp.split()
+a = int(arr[0])
+b = int(arr[1])
+
+print(f"{(a + b) / (a - b):.2f}")
