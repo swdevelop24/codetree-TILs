6c2bc4c992755c87fed1e118a5f9a1101c0fcd4a
swdevelop24 <154391675+swdevelop24@users.noreply.github.com>
2024-08-23 05:29:19 -0500

    Title: 특정 조건 두 정수 비교, Time: 147ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240823/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.py
@@ -0,0 +1,12 @@
+inp = input().split() 
+a = int(inp[0])
+b = int(inp[1]) 
+
+if a < b:
+    print(1, end=' ')
+else:
+    print(0, end=' ')
+if a==b:
+    print(1, end=' ')
+else:
+    print(0, end=' ')
