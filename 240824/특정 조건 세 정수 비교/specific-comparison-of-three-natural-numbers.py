970264c76576e66ee8cb4449602d89045a27cb19
swdevelop24 <154391675+swdevelop24@users.noreply.github.com>
2024-08-23 20:22:39 -0500

    Title: 특정 조건 세 정수 비교, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

--- 240824/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.py
+++ 240824/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.py
@@ -1,14 +1,12 @@
 arr=input().split()
-a,b,c=int(arr[0]), int(arr[1]), int(arr[2])
-d=0
-if a<=b and b<=c:
-    d=a
-elif a>b and b<c:
-    d=b
+a,b,c=int(arr[0]), int(arr[1]), int(arr[2]) 
+if a>=c and b>=c:
+    mini=c
+elif a>=b and c>=b:
+    mini=b
 else:
-    d=c
-
-if a == d:
+    mini =a 
+if a == mini:
     print(1, end=" ")
 else:
     print(0, end=" ")
