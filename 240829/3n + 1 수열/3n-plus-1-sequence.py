033060d704b82e7013e91f9aee8e661c42b74447
swdevelop24 <154391675+swdevelop24@users.noreply.github.com>
2024-08-28 21:50:46 -0500

    Title: 3n + 1 수열, Time: 122ms, Memory: 24MB, Status: Passed - CodeTree

--- 240829/3n + 1 수열/3n-plus-1-sequence.py
+++ 240829/3n + 1 수열/3n-plus-1-sequence.py
@@ -1,12 +1,12 @@
 N=int(input())
 cnt=0 
 while True: 
+    if N==1:
+        print(cnt)
+        break 
     if N%2==0:
         N//=2
     else:
         N*=3
         N+=1 
     cnt+=1
-    if N==1:
-        print(cnt)
-        break
