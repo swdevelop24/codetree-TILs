b8844df54f319d8c49312788ec93c65858465601
swdevelop24 <154391675+swdevelop24@users.noreply.github.com>
2024-08-30 23:47:18 -0500

    Title: 별표 출력하기 11, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

--- 240831/별표 출력하기 11/print-star-11.py
+++ 240831/별표 출력하기 11/print-star-11.py
@@ -2,7 +2,7 @@ n=int(input())
 
 for y in range(n*2+1):
     for x in range(n*2+1):
-        if y == 0 or y==n-1 or x==0 or x==n-1 or (y%2 ==0) or (x%2==0):
+        if (y%2 ==0) or (x%2==0):
             print("*", end=" ")
         else:
             print(" ", end=" ")
