d560f8b97cce539ef62f0462b5a421608e19d62a
swdevelop24 <154391675+swdevelop24@users.noreply.github.com>
2024-08-30 23:16:49 -0500

    Title: 별표 출력하기 10, Time: 105ms, Memory: 24MB, Status: Passed - CodeTree

--- 240831/별표 출력하기 10/print-star-10.py
+++ 240831/별표 출력하기 10/print-star-10.py
@@ -5,8 +5,7 @@ for i in range(n*2):
     if i%2==0:
         for _ in range((i//2)+1):
             print("*", end=" ")
-        print()
     else:
         for _ in range(n-((i-1)//2)):
             print("*", end=" ")
-        print()
+    print()
