748696e8e523bc3cecde852430a315d276a658f8
swdevelop24 <154391675+swdevelop24@users.noreply.github.com>
2024-08-30 21:42:37 -0500

    Title: 별 그리기, Time: 164ms, Memory: 27MB, Status: Passed - CodeTree

---
+++ 240831/별 그리기/star-drawing.py
@@ -0,0 +1,16 @@
+n =int(input())
+
+
+for y in range(n):
+    for v in range(n-y-1):
+        print(" ", end='')
+    for x in range(y*2+1):
+        print("*", end="")
+    print()
+
+for y in range(n-2,-1,-1):
+    for v in range(n-y-1):
+        print(" ", end='')
+    for x in range(y*2+1):
+        print("*", end="")
+    print()
