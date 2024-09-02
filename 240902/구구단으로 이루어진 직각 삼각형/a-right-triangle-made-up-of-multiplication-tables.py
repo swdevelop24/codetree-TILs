4287cf73ffdc6f4c8cf240dea0df3a1c841de1fa
swdevelop24 <154391675+swdevelop24@users.noreply.github.com>
2024-09-01 19:50:54 -0500

    Title: 구구단으로 이루어진 직각 삼각형, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240902/구구단으로 이루어진 직각 삼각형/a-right-triangle-made-up-of-multiplication-tables.py
@@ -0,0 +1,10 @@
+n=int(input())
+
+for i in range(1,n+1):
+    for x in range(1, n-i+2):
+        if x >1 and x <=n:
+            print("/", end=" ")
+            print(f"{i} * {x} = {i*x}",end=" ")
+        else:
+            print(f"{i} * {x} = {i*x}",end=" ")
+    print()
