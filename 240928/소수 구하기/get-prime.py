775a410fd8171b4e5818888426f0c3da106d6dc4
swdevelop24 <154391675+swdevelop24@users.noreply.github.com>
2024-09-27 14:56:01 -0500

    Title: 소수 구하기, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240928/소수 구하기/get-prime.py
+++ 240928/소수 구하기/get-prime.py
@@ -1,13 +1,10 @@
 n = int(input())
 
-if n == 2:
-    print(n, end=' ')
-else:
-    for i in range(2,n+1):
-        if n%i == 0:
-            if i == n:
-                print(i, end=' ') 
-            else:
-                continue
+for i in range(2,n+1):
+    if n%i == 0:
+        if i == n:
+            print(i, end=' ') 
         else:
-            print(i, end=' ')
+            continue
+    else:
+        print(i, end=' ')
