262396d3853f4a03d0e34043078fa0a2f4d04507
swdevelop24 <154391675+swdevelop24@users.noreply.github.com>
2024-09-27 20:21:39 -0500

    Title: 배열 채우기, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

--- 240928/배열 채우기/filling-array.py
+++ 240928/배열 채우기/filling-array.py
@@ -1,6 +1,18 @@
 points=list(map(int, input().split()))
 
+iszero=False
+idx=0 
 for i in range(len(points)):
     if points[i] == 0:
-        for x in range(i-1,-1,-1):
-            print(points[x], end=" ")
+        iszero = True
+        idx=i-1
+        break
+    
+
+if iszero: 
+    for x in range(idx, -1,-1):
+        print(points[x], end=" ")
+    
+else:
+    for x in range(len(points)-1,-1,-1):
+        print(points[x], end=" ")
