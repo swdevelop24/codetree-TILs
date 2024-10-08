0ec98fb767153daa772fa7634edb4dc958342bce
swdevelop24 <154391675+swdevelop24@users.noreply.github.com>
2024-10-08 11:24:38 -0500

    Title: 개수 세기, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

--- 241009/개수 세기/count-numbers.py
+++ 241009/개수 세기/count-numbers.py
@@ -2,8 +2,11 @@ n, m = tuple(map(int, input().split()))
 
 arr=list(map(int, input().split()))
 
+'''
 cnt=0 
 for num in arr:
     if num == m:
         cnt+=1 
 print(cnt)
+'''
+print(arr.count(m))
