449bd1523a84973d52c35c748e9179eb6985619f
swdevelop24 <154391675+swdevelop24@users.noreply.github.com>
2024-10-07 22:57:57 -0500

    Title: 전항의 두 배, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 241008/전항의 두 배/twice-the-previous.py
+++ 241008/전항의 두 배/twice-the-previous.py
@@ -1,7 +1,10 @@
 arr=input().split()
 a,b = int(arr[0]), int(arr[1])
 
-newlist=[a,b]
+
+#newlist=[a,b]
+newlist.append(a)
+newlist.append(b)
 
 for i in range(2,10):
     x = newlist[i-1]+ newlist[i-2]*2
