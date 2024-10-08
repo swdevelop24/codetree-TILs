9ce6e38f3820e1d6f440d0e09447ae2696bb9fe4
swdevelop24 <154391675+swdevelop24@users.noreply.github.com>
2024-10-08 12:58:33 -0500

    Title: 연속부분수열일까, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241009/연속부분수열일까/contiguous-array-or-not.py
+++ 241009/연속부분수열일까/contiguous-array-or-not.py
@@ -2,11 +2,32 @@ n1, n2 = map(int, input().split())
 arr = list(map(int, input().split()))
 brr = list(map(int, input().split()))
 
+'''
 flag = False
 for i in range(n1 - n2 + 1):  # `arr`에서 `brr`를 검사할 수 있는 모든 시작 지점을 순회
     if arr[i:i+n2] == brr:    # `arr`의 부분 배열이 `brr`와 일치하는지 확인
         flag = True
         break
+
+if flag:
+    print("Yes")
+else:
+    print("No")
+''' 
+
+flag = True
+for i in range(len(arr)):
+    for x in range(len(brr)):
+        if arr[i+x] != brr[x]:
+            flag=False
+            break
+        if i+x >= len(arr):
+            flag=False
+            break
+        
+    if not flag:
+        break 
+
 
 if flag:
     print("Yes")
