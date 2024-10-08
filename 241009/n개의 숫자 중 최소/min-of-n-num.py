a821c4154b27638f5bcaadf9ec3bab4250a2b324
swdevelop24 <154391675+swdevelop24@users.noreply.github.com>
2024-10-08 14:18:45 -0500

    Title: n개의 숫자 중 최소, Time: 105ms, Memory: 24MB, Status: Passed - CodeTree

--- 241009/n개의 숫자 중 최소/min-of-n-num.py
+++ 241009/n개의 숫자 중 최소/min-of-n-num.py
@@ -2,12 +2,14 @@ import sys
 n=int(input())
 arr=list(map(int, input().split()))
 
-cnt=[0]*101 
 
+cnt=0 
 mini = sys.maxsize 
 for num in arr:
-    if mini >= num:
+    if mini > num:
         mini = num
-        cnt[num]+=1
+        cnt=1
+    elif mini== num:
+        cnt+=1
 
-print(mini, cnt[mini])
+print(mini, cnt)
