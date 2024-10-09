f26c27fd029a0d96616401f65492566afa5f9960
swdevelop24 <154391675+swdevelop24@users.noreply.github.com>
2024-10-08 20:34:14 -0500

    Title: 대문자로 바꾸기, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

--- 241009/대문자로 바꾸기/change-to-capital.py
+++ 241009/대문자로 바꾸기/change-to-capital.py
@@ -8,3 +8,13 @@ for y in range(5):
     for x in range(3):
         print(arr[y][x], end=' ')
     print()
+
+
+''' 
+아스키 코드 사용법
+for i in range(5):
+	for j in range(3):
+		arr_2d[i][j] = chr(ord(arr_2d[i][j]) + ord('A') - ord('a'))
+		print(arr_2d[i][j], end=" ")
+	print()
+'''
