1906b964b6490624d633b71003eb49b3e8a95c64
swdevelop24 <154391675+swdevelop24@users.noreply.github.com>
2024-10-08 21:08:59 -0500

    Title: 배열의 평균, Time: 120ms, Memory: 24MB, Status: Passed - CodeTree

--- 241009/배열의 평균/ave-of-array.py
+++ 241009/배열의 평균/ave-of-array.py
@@ -19,3 +19,36 @@ for y in range(2):
     for x in range(4):
         tavg+=arr[y][x]
 print(f"{tavg/8:.1f}", end=' ')
+
+
+'''
+
+# 2차원 배열을 구현해 각 줄마다 정수를 입력받습니다.
+arr_2d = [
+	list(map(int, input().split()))
+	for _ in range(2)
+]
+
+# 가로 평균
+for i in range(2):
+	sum_val = 0
+	for j in range(4):
+		sum_val += arr_2d[i][j]
+	print(f"{sum_val / 4:.1f}", end=" ")
+print()
+
+# 세로 평균
+for j in range(4):
+	sum_val = 0
+	for i in range(2):
+		sum_val += arr_2d[i][j]
+	print(f"{sum_val / 2:.1f}", end=" ")
+print()
+
+# 전체 평균
+sum_val = 0
+for i in range(4):
+	for j in range(2):
+		sum_val += arr_2d[j][i]
+print(f"{sum_val / 8:.1f}")
+'''
