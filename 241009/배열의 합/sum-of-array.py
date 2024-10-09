42e84a23c47125823515e23bb7ed5021edfb22fe
swdevelop24 <154391675+swdevelop24@users.noreply.github.com>
2024-10-08 20:24:11 -0500

    Title: 배열의 합, Time: 163ms, Memory: 24MB, Status: Passed - CodeTree

--- 241009/배열의 합/sum-of-array.py
+++ 241009/배열의 합/sum-of-array.py
@@ -1,3 +1,11 @@
+'''
 for i in range(4):  
     arr=list(map(int, input().split()))
     print(sum(arr))
+'''
+
+# using list comprehension - again 
+arr=[list(map(int, input().split())) for _ in range(4)]
+
+for i in range(4):
+    print(sum(arr[i]))
