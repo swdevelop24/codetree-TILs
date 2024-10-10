4483c3136c2bb650561901f69e6197f99bc6065b
swdevelop24 <154391675+swdevelop24@users.noreply.github.com>
2024-10-09 20:43:25 -0500

    Title: 더 긴 문자열, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

--- 241010/더 긴 문자열/longer-string.py
+++ 241010/더 긴 문자열/longer-string.py
@@ -3,5 +3,8 @@ len1 = len(str1)
 len2 =len(str2) 
 if len1 == len2:
     print("same")
-
-print(str1, len1) if len1 > len2 else print(str2, len2)
+elif len1 > len2:
+    print(str1, len1)  
+    
+else:
+    print(str2, len2)
