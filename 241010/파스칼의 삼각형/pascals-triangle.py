073033939ffc20b81a925eaf9cdd0af2a79586d5
swdevelop24 <154391675+swdevelop24@users.noreply.github.com>
2024-10-09 19:08:44 -0500

    Title: 파스칼의 삼각형, Time: 147ms, Memory: 24MB, Status: Passed - CodeTree

--- 241010/파스칼의 삼각형/pascals-triangle.py
+++ 241010/파스칼의 삼각형/pascals-triangle.py
@@ -12,6 +12,19 @@ for y in range(1,n):
         else:
             arr[y][x] =  arr[y-1][x] + arr[y-1][x-1]
 
+'''
+	
+# 배열의 각 행의 첫 열과 마지막 열을 1로 초기!! 요부분 잘 확인하기 
+for i in range(n):
+	arr[i][0] = 1
+	arr[i][i] = 1
+	
+# 배열의 숫자를 채우기 이게 더 쉬움.. 
+for i in range(n):
+	for j in range(1, i):
+		arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j]
+'''
+
 
 for row in arr:
     for elem in row:
