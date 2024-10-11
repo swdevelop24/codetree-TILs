250aa95fbe5489bdb3ec6a75edf213c2b5597f04
swdevelop24 <154391675+swdevelop24@users.noreply.github.com>
2024-10-10 21:41:45 -0500

    Title: 문자열 범위 출력하기 2, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241011/문자열 범위 출력하기 2/print-string-in-range-2.py
+++ 241011/문자열 범위 출력하기 2/print-string-in-range-2.py
@@ -1,9 +1,13 @@
 arr = input()
 n =int(input())
 
-num = len(arr)-1-n
-if num < 0:
-    num=1
+num = len(arr)
+cnt=0 
 
-for x in range(len(arr)-1,num, -1):
-    print(arr[x], end='')
+# edge case 에 유의하기=> 몇개 인지 모르기 맨 마지막까지 순회!!! 
+# 1개인 경우에 에러 날수 있음. 
+for x in range(len(arr)-1,-1, -1):
+    if cnt >=n:
+        break
+    print(arr[x], end=' ')
+    cnt+=1
