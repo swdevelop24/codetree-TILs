5866338026acb4b7eea3bf6eb79b6e93b1bfef27
swdevelop24 <154391675+swdevelop24@users.noreply.github.com>
2024-10-14 15:49:37 -0500

    Title: 문자열의 특정 위치 찾기, Time: 145ms, Memory: 24MB, Status: Passed - CodeTree

--- 241015/문자열의 특정 위치 찾기/find-specific-location-in-spring.py
+++ 241015/문자열의 특정 위치 찾기/find-specific-location-in-spring.py
@@ -1,6 +1,22 @@
+'''
 arr=input().split()
 ret = arr[0].find(arr[1])
 if ret == -1:
     print("No")
 else:
     print(ret)
+'''
+
+string , a = tuple(input().split()) 
+idx=-1 
+length=len(string)
+
+for i in range(length):
+    if string[i] == a:
+        idx = i 
+        break
+
+if idx ==-1:
+    print("No")
+else:
+    print(idx)
