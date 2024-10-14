6347c057f2161360007d9f17e41d8c9f720e278b
swdevelop24 <154391675+swdevelop24@users.noreply.github.com>
2024-10-14 12:21:07 -0500

    Title: 문자열 전부 붙이기, Time: 105ms, Memory: 24MB, Status: Passed - CodeTree

--- 241015/문자열 전부 붙이기/paste-all-string.py
+++ 241015/문자열 전부 붙이기/paste-all-string.py
@@ -5,3 +5,19 @@ for i in range(n):
     arr+=input()
 
 print(arr)
+
+'''
+#diffrent method
+string = [
+	input()
+	for _ in range(n)
+]
+str_all = ""
+
+# 문자열을 전부 붙임
+for i in range(n):
+	str_all += string[i]
+	
+# 합쳐진 문자열을 출력
+print(str_all)
+'''
