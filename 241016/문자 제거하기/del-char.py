ac911be4ab8316be876f89da5723d07448c920ae
swdevelop24 <154391675+swdevelop24@users.noreply.github.com>
2024-10-15 15:02:53 -0500

    Title: 문자 제거하기, Time: 145ms, Memory: 24MB, Status: Passed - CodeTree

--- 241016/문자 제거하기/del-char.py
+++ 241016/문자 제거하기/del-char.py
@@ -11,3 +11,15 @@ while currlen >1:
         string.pop(idx)
     currlen = len(string)
     print(''.join(string))
+
+
+'''
+# 정수가 문자열의 길이 이상이면 마지막 문자를 가리키게 변경
+	if a >= leng:
+		a = leng - 1
+		
+	arr.pop(a)
+	leng -= 1
+
+	string = ''.join(arr)
+'''
