a537488399a22bc0def1f71c560a9fe9dd1b9835
swdevelop24 <154391675+swdevelop24@users.noreply.github.com>
2024-10-15 12:03:48 -0500

    Title: 첫 번째와 두 번째 교환, Time: 144ms, Memory: 24MB, Status: Passed - CodeTree

--- 241016/첫 번째와 두 번째 교환/exchange-1st-and-2nd.py
+++ 241016/첫 번째와 두 번째 교환/exchange-1st-and-2nd.py
@@ -8,4 +8,14 @@ for x in range(len(arr)):
     elif arr[x] == second:
         arr[x] = first 
 
-print(''.join(arr))
+print(''.join(arr)) 
+
+
+'''
+# 문자열을 순회하면서 첫 번째 문자와 두 번째 문자를 교환
+for i in range(len(string)):
+	if string[i] == a:
+		string = string[:i] + b + string[i + 1:]
+	elif string[i] == b:
+		string = string[:i] + a + string[i + 1:]
+'''
