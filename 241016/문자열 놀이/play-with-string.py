f11a2c8398ab2555347f90f9eee5aad3d6035b22
swdevelop24 <154391675+swdevelop24@users.noreply.github.com>
2024-10-15 14:14:05 -0500

    Title: 문자열 놀이, Time: 137ms, Memory: 73MB, Status: Passed - CodeTree

--- 241016/문자열 놀이/play-with-string.py
+++ 241016/문자열 놀이/play-with-string.py
@@ -29,3 +29,33 @@ for _ in range(q):
 
     # 쿼리 수행 후 결과 출력
     print(string)
+
+
+'''
+# 입력 받기
+string, q = input().split()
+q = int(q)  # 쿼리 개수를 정수로 변환
+
+# 각 쿼리 처리
+for _ in range(q):
+    arr = input().split()
+    operation = int(arr[0])  # 쿼리 유형 (1: 교환, 2: 문자 대체)
+
+    if operation == 1:
+        ath = int(arr[1]) - 1  # 첫 번째 인덱스
+        bth = int(arr[2]) - 1  # 두 번째 인덱스
+
+        # 문자열을 리스트로 변환 후 인덱스의 문자 교환
+        string = list(string)
+        string[ath], string[bth] = string[bth], string[ath]
+        string = ''.join(string)
+
+    elif operation == 2:
+        old_char = arr[1]  # 대체할 문자
+        new_char = arr[2]  # 새로운 문자
+        # replace를 사용하여 문자 치환
+        string = string.replace(old_char, new_char)
+
+    # 쿼리 수행 후 결과 출력
+    print(string)
+'''
