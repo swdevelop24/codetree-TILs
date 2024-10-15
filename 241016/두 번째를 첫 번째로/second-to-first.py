e547e6a57342edfaa356b5cc0eac607feb89292b
swdevelop24 <154391675+swdevelop24@users.noreply.github.com>
2024-10-15 14:23:53 -0500

    Title: 두 번째를 첫 번째로, Time: 217ms, Memory: 24MB, Status: Passed - CodeTree

--- 241016/두 번째를 첫 번째로/second-to-first.py
+++ 241016/두 번째를 첫 번째로/second-to-first.py
@@ -8,3 +8,11 @@ for x in range(len(arr)):
         arr[x] = fir
 
 print(''.join(arr))
+
+
+'''
+# 문자열을 순회하면서 두 번째 문자를 첫 번째 문자로 교환하는 방법.
+for i in range(len(string)):
+	if string[i] == b:
+		string = string[:i] + a + string[i + 1:]
+'''
