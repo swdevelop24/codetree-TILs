a6b7621d8f10c63d39062e6b583bde1d85b5abde
swdevelop24 <154391675+swdevelop24@users.noreply.github.com>
2024-10-15 11:48:13 -0500

    Title: a로 채워넣기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 241016/a로 채워넣기/filling-with-a.py
+++ 241016/a로 채워넣기/filling-with-a.py
@@ -1,12 +1,15 @@
-'''
 arr=list(input())
 
 arr[1]='a'
 arr[-2]='a'
 
 print(''.join(arr))
-'''
 
-arr=input()
-arr=arr[:1]+'a'+arr[2:-2]+'a'+arr[-1:]
-print(arr)
+
+'''
+leng = len(string)
+
+# 문자열의 앞에서 두 번째 원소와 뒤에서 두 번째 원소를 'a'로 대체합니다.
+string = string[:1] + 'a' + string[2:]
+string = string[:leng - 2] + 'a' + string[leng - 1:]
+''
