99517ef24f7447902e73506f3a8ed7774270495a
swdevelop24 <154391675+swdevelop24@users.noreply.github.com>
2024-10-15 10:46:44 -0500

    Title: 부분 문자열의 개수, Time: 139ms, Memory: 24MB, Status: Passed - CodeTree

--- 241016/부분 문자열의 개수/number-of-substrings.py
+++ 241016/부분 문자열의 개수/number-of-substrings.py
@@ -1,5 +1,4 @@
 a,b = input(), input()
-idx=-1 
 
 alen=len(a)
 blen=len(b)
@@ -7,10 +6,7 @@ cnt=0
 
 for i in range(alen-1):
     if a[i:i+2] == b:
-        idx = i 
         cnt+=1
 
-if idx ==-1:
-    print(0)
-else:
-    print(cnt)
+
+print(cnt)
