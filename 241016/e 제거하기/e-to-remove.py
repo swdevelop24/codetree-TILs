29467b64402611a5053d3b5a2c49837c441c7404
swdevelop24 <154391675+swdevelop24@users.noreply.github.com>
2024-10-15 16:10:44 -0500

    Title: e 제거하기, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

--- 241016/e 제거하기/e-to-remove.py
+++ 241016/e 제거하기/e-to-remove.py
@@ -3,3 +3,25 @@ idx = arr.find('e')
 
 arr = arr[:idx] + arr[idx+1:]
 print(arr)
+
+
+'''
+
+# pop 함수를 사용하기 위해 문자열을 list로 전환
+arr = list(string)
+leng = len(arr)
+	
+# 문자열을 순회하며 e가 있는지 탐색
+for i in range(leng):
+    if arr[i] == 'e':
+        # 해당 문자열이 e라면 그 원소를 제거
+        arr.pop(i)
+        leng -= 1
+        break
+
+# list를 문자열로 변환.
+string = ''.join(arr)
+
+# e가 제거된 문자열을 출력
+print(string)
+'''
