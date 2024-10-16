210ccf55b6dde28c23906251b534e384f33fc935
swdevelop24 <154391675+swdevelop24@users.noreply.github.com>
2024-10-16 17:00:20 -0500

    Title: 미는 횟수, Time: 118ms, Memory: 25MB, Status: Passed - CodeTree

--- 241017/미는 횟수/number-of-pushes.py
+++ 241017/미는 횟수/number-of-pushes.py
@@ -4,12 +4,12 @@ brr = input()
 n = 0 
 
 while True:
-    # 회전: 첫 번째 문자를 마지막으로 이동
+    # 회전:
     arr = list(arr)  # arr을 리스트로 변환
-    temp = arr[0]  # 첫 번째 문자 저장
-    for x in range(len(arr) - 1):
-        arr[x] = arr[x + 1]  # 각 문자를 왼쪽으로 이동
-    arr[-1] = temp  # 첫 번째 문자를 마지막으로
+    temp = arr[-1]  
+    for x in range(len(arr) - 1, 0,-1):
+        arr[x] = arr[x - 1]  
+    arr[0] = temp  # 첫 번째 문자를 마지막으로
 
     n += 1
     
