817bf3d2b52d9a2b77bc3ec279295f7f06fbd6de
swdevelop24 <154391675+swdevelop24@users.noreply.github.com>
2024-10-15 23:41:11 -0500

    Title: 규칙에 따라 밀기, Time: 124ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 241016/규칙에 따라 밀기/push-by-the-rules.py
@@ -0,0 +1,16 @@
+arr=input() 
+order=input()
+
+for x in range(len(order)):
+    #shift to the left 
+    if order[x] =='L':
+        temp = arr[0]
+        arr=arr[1:len(arr)] + temp 
+    
+
+    else:
+        temp = arr[-1]
+        arr=temp + arr[:-1]
+    
+
+print(arr)
