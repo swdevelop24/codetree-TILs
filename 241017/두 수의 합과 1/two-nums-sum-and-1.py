9095fa55d697412b4f468bc433e11a1942ec9c4b
swdevelop24 <154391675+swdevelop24@users.noreply.github.com>
2024-10-16 15:21:47 -0500

    Title: 두 수의 합과 1, Time: 131ms, Memory: 24MB, Status: Passed - CodeTree

--- 241017/두 수의 합과 1/two-nums-sum-and-1.py
+++ 241017/두 수의 합과 1/two-nums-sum-and-1.py
@@ -3,8 +3,8 @@ a, b =map(int, input().split())
 sum = str(a+b)
 cnt=0 
 for elem in sum:
-    if elem >='0' and elem <'9':
-        if elem == '1':
-            cnt+=1
+    # no need if elem >='0' and elem <'9':
+    if elem == '1':
+        cnt+=1
 
 print(cnt)
