92d37c38e98c46d09d2895591d3a0818a67e8464
swdevelop24 <154391675+swdevelop24@users.noreply.github.com>
2024-10-16 12:09:59 -0500

    Title: 정수만 더하기, Time: 120ms, Memory: 24MB, Status: Passed - CodeTree

--- 241017/정수만 더하기/add-only-integers.py
+++ 241017/정수만 더하기/add-only-integers.py
@@ -6,3 +6,10 @@ for elem in arr:
         sum+=int(elem)
 
 print(sum)
+
+
+'''
+for elem in string:
+    if ord(elem) >= ord('0') and ord(elem) <= ord('9'):
+        ans += ord(elem) - ord('0')
+'''
