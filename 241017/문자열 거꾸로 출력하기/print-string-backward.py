1879fe6790d6900a674e1068cb966e698834617b
swdevelop24 <154391675+swdevelop24@users.noreply.github.com>
2024-10-16 16:27:48 -0500

    Title: 문자열 거꾸로 출력하기, Time: 170ms, Memory: 24MB, Status: Passed - CodeTree

--- 241017/문자열 거꾸로 출력하기/print-string-backward.py
+++ 241017/문자열 거꾸로 출력하기/print-string-backward.py
@@ -6,4 +6,16 @@ while flag:
         break
     
     string=string[-1:0:-1] +string[0]
+
     print(string)
+
+
+    '''
+    # 문자열의 길이.
+	leng = len(string)
+		
+	# 문자열을 역으로 출력.
+	for i in range(leng - 1, -1, -1):
+		print(string[i], end="")
+	print()
+    '''
