7979dbba922e779606ec3b2571aa992d7d17b694
swdevelop24 <154391675+swdevelop24@users.noreply.github.com>
2024-10-16 15:00:45 -0500

    Title: 알파벳 지우기, Time: 159ms, Memory: 24MB, Status: Passed - CodeTree

--- 241017/알파벳 지우기/remove-alphabet.py
+++ 241017/알파벳 지우기/remove-alphabet.py
@@ -16,3 +16,16 @@ for elem in brr:
 		num2+=elem
 
 print(int(num1)+int(num2))
+
+
+'''
+# a의 정수로 변환 가능한 부분을 다른 문자열로 옮김
+for elem in a:
+	if ord(elem) <= ord('9') and ord(elem) >= ord('0'):
+		str1 += elem
+	
+# b의 정수로 변환 가능한 부분을 다른 문자열로 옮김 
+for elem in b:
+	if ord(elem) <= ord('9') and ord(elem) >= ord('0'):
+		str2 += elem
+'''
