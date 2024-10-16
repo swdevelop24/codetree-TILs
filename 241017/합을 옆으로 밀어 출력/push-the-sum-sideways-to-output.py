5cc128705736a37f7178726eaa5b620fb207db77
swdevelop24 <154391675+swdevelop24@users.noreply.github.com>
2024-10-16 16:07:53 -0500

    Title: 합을 옆으로 밀어 출력, Time: 99ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241017/합을 옆으로 밀어 출력/push-the-sum-sideways-to-output.py
@@ -0,0 +1,16 @@
+n=int(input())
+total=0 
+for _ in range(n):
+    arr=int(input())
+    total += arr
+
+total = list(str(total))
+
+temp = total[0]
+
+for i in range(len(total)):
+    total[i-1] = total[i] 
+
+total[len(total)-1] = temp 
+
+print(''.join(total))
