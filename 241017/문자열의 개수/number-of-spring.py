d6bd1d4f07875883173fe94b4adc583b5f52d8dd
swdevelop24 <154391675+swdevelop24@users.noreply.github.com>
2024-10-16 16:38:49 -0500

    Title: 문자열의 개수, Time: 106ms, Memory: 24MB, Status: Passed - CodeTree

--- 241017/문자열의 개수/number-of-spring.py
+++ 241017/문자열의 개수/number-of-spring.py
@@ -15,3 +15,25 @@ while flag:
 print(cnt)
 for elem in ret:
     print(elem)
+
+
+'''
+# 0이 나올때까지 반복
+while True:
+	# 문자열을 입력
+	inp = input()
+	string.append(inp)
+		
+	# 0이 나올 경우 while문을 빠져나옴
+	if string[cnt] == "0":
+		break
+		
+	cnt += 1
+	
+# 문자열의 개수를 출력
+print(cnt)
+	
+# 홀수 번째 문자열을 출력
+for i in range(0, cnt, 2):
+	print(string[i])
+'''
