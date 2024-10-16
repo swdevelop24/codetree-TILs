d9c7446b4709ddf4e86d071d31aa4081d85c3194
swdevelop24 <154391675+swdevelop24@users.noreply.github.com>
2024-10-16 14:42:58 -0500

    Title: 정수만 추출하기, Time: 124ms, Memory: 24MB, Status: Passed - CodeTree

--- 241017/정수만 추출하기/extract-only-integers.py
+++ 241017/정수만 추출하기/extract-only-integers.py
@@ -1,7 +1,7 @@
 a, b = input().split()
 
-alast = 0
-blast = 0
+alast = len(a)
+blast = len(b)
 
 # 첫 번째 숫자가 아닌 문자의 위치 찾기
 for x in range(len(a)):
@@ -19,3 +19,26 @@ sum1 = a[:alast]
 sum2 = b[:blast]
 
 print(int(sum1) + int(sum2))
+
+#에지 케이스에 유의하기 (전부다 디짓인 경우 )
+'''
+idx1 = 0
+idx2 = 0
+
+# a의 정수로 변환 가능한 최대 인덱스를 찾기
+for elem in a:
+	if elem <= '9' and elem >= '0':
+		idx1 += 1
+	else:
+		break
+
+# b의 정수로 변환 가능한 최대 인덱스를 찾기 .
+for elem in b:
+	if elem <= '9' and elem >= '0':
+		idx2 += 1
+	else:
+		break
+	
+str1 = a[:idx1]
+str2 = b[:idx2]
+'''
