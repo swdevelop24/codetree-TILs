df3c13819a125446e3fa16bae94f29be67284731
swdevelop24 <154391675+swdevelop24@users.noreply.github.com>
2024-10-16 14:13:33 -0500

    Title: 정수만 추출하기, Time: 108ms, Memory: 25MB, Status: Passed - CodeTree

--- 241017/정수만 추출하기/extract-only-integers.py
+++ 241017/정수만 추출하기/extract-only-integers.py
@@ -1,19 +1,21 @@
-a, b = input().split() 
+a, b = input().split()
 
-alast=0
-blast=0 
+alast = len(a)
+blast = len(b)
+
+# 첫 번째 숫자가 아닌 문자의 위치 찾기
 for x in range(len(a)):
     if not a[x].isdigit():
-        alast=x
+        alast = x
         break
-
 
 for x in range(len(b)):
     if not b[x].isdigit():
-        blast=x
+        blast = x
         break
 
+# 문자열의 앞 부분을 숫자로 변환하여 더하기
 sum1 = a[:alast]
-sum2 =b[:blast]
+sum2 = b[:blast]
 
 print(int(sum1) + int(sum2))
