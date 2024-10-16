c45066bd385f6df0c7518233e527cb539c743388
swdevelop24 <154391675+swdevelop24@users.noreply.github.com>
2024-10-16 16:46:52 -0500

    Title: 미는 횟수, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241017/미는 횟수/number-of-pushes.py
+++ 241017/미는 횟수/number-of-pushes.py
@@ -1,15 +1,20 @@
-arr=input()
-brr=input()
+arr = input()
+brr = input()
 
-n=0 
+n = 0 
 
 while True:
-    arr=arr[1:-1] + arr[-1] + arr[0]
-    n+=1 
+    # 회전: 첫 번째 문자를 마지막으로 이동
+    arr = arr[1:] + arr[0]
+    n += 1 
+    
+    # arr이 brr과 같아질 경우
     if arr == brr:
         break 
+    
+    # n이 len(arr)와 같아지면 회전을 중단
     if n == len(arr):
-        n=-1
+        n = -1
         break
 
 print(n)
