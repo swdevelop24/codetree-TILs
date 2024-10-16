ab7fef08a0aaa61fd11f2d1a3fc5dd774446ba33
swdevelop24 <154391675+swdevelop24@users.noreply.github.com>
2024-10-16 00:09:22 -0500

    Title: 그 전 알파벳, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 241016/그 전 알파벳/before-the-alphabet.py
+++ 241016/그 전 알파벳/before-the-alphabet.py
@@ -1,7 +1,6 @@
 arr=input()
 
-if arr >='a' and arr<='z':
-    if arr == 'a':
-        print(z)
-    else:
-        print(chr(ord(arr)-1))
+if arr == 'a':
+    print(z)
+else:
+    print(chr(ord(arr)-1))
