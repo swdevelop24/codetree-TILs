24b3a40a7412167261300104f6dc90047164db5d
swdevelop24 <154391675+swdevelop24@users.noreply.github.com>
2024-12-17 22:21:08 -0600

    Title: 별 찍는 것을 5번 반복하기, Time: 39ms, Memory: 16MB, Status: Passed - CodeTree

--- 241218/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.py
+++ 241218/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.py
@@ -1,11 +1,6 @@
 def print_10_stars():
-    for _ in range(10):
-        print("*", end='')
+    print("*"*10)
     
 
-def repeat():
-    for _ in range(5):
-        print_10_stars()
-        print()
-
-repeat()
+for _ in range(5):
+    print_10_stars()
