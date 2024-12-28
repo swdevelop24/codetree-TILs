bb494b49c38116adeca1a53b9a82077cb45c4a45
swdevelop24 <154391675+swdevelop24@users.noreply.github.com>
2024-12-27 19:59:14 -0600

    Title: 홀수 짝수에 따른 출력값, Time: 42ms, Memory: 16MB, Status: Passed - CodeTree

--- 241228/홀수 짝수에 따른 출력값/output-value-based-on-odd-even-numbers.py
+++ 241228/홀수 짝수에 따른 출력값/output-value-based-on-odd-even-numbers.py
@@ -11,3 +11,17 @@ def dfs(num):
         return dfs(num-2) + num 
 
 print(dfs(n))
+
+'''
+def get_num(n):
+    if n == 1:
+        return 1
+    if n == 2:
+        return 2
+
+    # n과 홀짝이 같은 수만을 재귀함수로 호출.
+    return get_num(n - 2) + n
+
+
+print(get_num(n))
+'''
