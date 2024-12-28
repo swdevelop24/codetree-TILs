77acf47a41b33ecb0f5ad1fd54cf08b6d2164016
swdevelop24 <154391675+swdevelop24@users.noreply.github.com>
2024-12-27 21:41:40 -0600

    Title: 재귀함수를 이용한 3n + 1 수열, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 241228/재귀함수를 이용한 3n + 1 수열/3n-plus-1-sequence-with-recursive-function.py
@@ -0,0 +1,14 @@
+n=int(input())
+
+
+def dfs(num):
+    if num==1:
+        return 0
+
+    if num%2==0:
+        return dfs(num//2) +1
+    else:
+        return dfs(num*3 +1)+1
+    
+
+print(dfs(n))    
